b38db8831dd288c6d994eb95784a02b2a8995ecd
Dishanthan2001 <sdishanthanpaul1612@gmail.com>
2021-08-07 12:57:51 +0530

    Modified File

--- myFirst_repo/Program.cs
+++ myFirst_repo/Program.cs
@@ -11,6 +11,10 @@ namespace Practise
         {
             string phrase = "my name is Dishanthan ";
             Console.WriteLine(phrase);
+            Console.WriteLine("What is your age? ");
+            Console.ReadLine(); 
+
+
         }
     }
 }
