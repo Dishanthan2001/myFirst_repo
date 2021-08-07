47ae7eb8b6600dc0d7ae8a09101ed50025096f99
Dishanthan2001 <sdishanthanpaul1612@gmail.com>
2021-08-07 12:42:45 +0530

    REPO demo

---
+++ myFirst_repo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+using System;
+
+namespace Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "my name is Dishanthan ";
+            Console.WriteLine(phrase);
+        }
+    }
+}
